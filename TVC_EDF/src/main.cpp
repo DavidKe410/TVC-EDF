a3ff3a338df591a2cded3e4d22b35fc82f20e064
David Ke <davidke7@gmail.com>
2026-01-28 13:58:53 -0700

    just more updates on getting both sensors to work

--- TVC_EDF/src/main.cpp
+++ TVC_EDF/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Adafruit_BNO08x.h>
+#include <Wire.h>
+#include "SparkFun_ISM330DHCX.h"
 #include "RingBuf.h"
 #include "SdFat.h"
 
@@ -11,11 +13,15 @@ short loopCount = 0;
 // ====== End General ======
 
 //====== BNO085 ======
-#define BNO08X_RESET -1
-Adafruit_BNO08x  bno08x(BNO08X_RESET);
+Adafruit_BNO08x  bno08x(-1);
 sh2_SensorValue_t sensorValue;
 long reportIntervalUs = 2000;
 //====== End BNO085 ======
+
+// ======= ISM330DLC =======
+SparkFun_ISM330DHCX ism330;
+sfe_ism_data_t accelData;
+// ======= End ISM330DLC =======
 
 //======= Data Logger ========
 #define SD_CONFIG SdioConfig(FIFO_SDIO)
@@ -49,7 +55,9 @@ struct IMUData {
     float i;
     float j;
     float k;
-    short cali_status;
+    short temp;
+    bool new_accel;
+    short orien_cali_status;
 };
 
 // Struct to consolidate all data
@@ -67,39 +75,116 @@ void logData(AllData &data);
 //void readReceiver(DuplexData &data);
 void setReports(sh2_SensorId_t reportType, long report_interval);
 void readBNO085(IMUData &data);
+void setupISM330();
+void readISM330(IMUData &data);
 
 void setup() {
+
     Serial.begin(115200);
     while (!Serial) delay(10); // will pause until serial console opens
 
     setupSD();
 
-    while (!bno08x.begin_I2C()) {
+    while (!bno08x.begin_I2C()) { // This starts a wire that the ISM can use...AND overwrites the setClock speed to soemthing slower????wtf is wrong with this sensor
       Serial.println("Failed to find BNO08x chip");
       delay(500);
     }
+    Wire.setClock(400000);
+    setupISM330();
+
     delay(3000);
 }
-int count = 0;
 
+int count = 0;
+int validAccel = 0;
+int validOrien = 0;
 void loop() {
     unsigned long startMillis = millis();
     //readReceiver(all_data.duplex);
-
-    while (count < 500) {
+    while (count < 100) {
+        delayMicroseconds(500);
+        //readISM330(all_data.imu);
         readBNO085(all_data.imu);
         //logData(all_data);
         //delay(2000);
     }
     unsigned long endMillis = millis();
-    Serial.println("time taken: " + String(1000/((endMillis - startMillis)/500.0), 4));
-    count = 0;
+    Serial.println("time taken: " + String(1000/((endMillis - startMillis)/100.0), 4));
+    Serial.println("Valid Accel: " + String(validAccel) + ". Valid Orien: " + String(validOrien));
     //cleanupSD(); // Cleanup SD card
-    delay(1000);
+    count = 0;
+    delay(3000);
     //setupSD(); // Setup SD card again for next logging session
 }
 
 //============================================ Functions ============================================
+void setupISM330() {
+	while (!ism330.begin()){
+		Serial.println("ISM did not begin. Please check the wiring...");
+		delay(500);
+	}
+    ism330.deviceReset();
+	// Wait for it to finish reseting
+	while (!ism330.getDeviceReset()){delay(1);}
+    delay(100);
+ 	ism330.setDeviceConfig();
+	ism330.setBlockDataUpdate();   
+
+	ism330.setAccelDataRate(ISM_XL_ODR_1666Hz);
+	ism330.setAccelFullScale(ISM_4g);
+	ism330.setGyroDataRate(ISM_GY_ODR_OFF); // Not planning to use gyro data atm
+
+	// Turn on the accelerometer's filter and apply settings.
+	ism330.setAccelFilterLP2();
+	ism330.setAccelSlopeFilter(ISM_LP_ODR_DIV_400);
+    Serial.println("ISM330DHCX setup complete.");
+}
+
+void readISM330(IMUData &data) {
+	if( ism330.checkAccelStatus() ){
+		ism330.getAccel(&accelData);
+        data.accel_x = accelData.xData*0.00980665; // Convert mg to m/s²
+        data.accel_y = accelData.yData*0.00980665;
+        data.accel_z = accelData.zData*0.00980665;
+        data.temp = ism330.getTemp()/256.0 + 25; // Convert to °C
+        data.new_accel = true;
+        validAccel++;
+        //count++;
+    }else{
+        data.new_accel = false; // Old data
+    }
+}
+
+void readBNO085(IMUData &data) {
+    if (bno08x.wasReset()) {
+        Serial.println("BNO085 was reset.");
+        setReports(SH2_ROTATION_VECTOR, 2000);
+        delay(300);
+    }
+    if (bno08x.getSensorEvent(&sensorValue)) {
+        data.orien_cali_status = sensorValue.status;
+        switch (sensorValue.sensorId) {
+            case SH2_ROTATION_VECTOR:
+                data.real = sensorValue.un.rotationVector.real;
+                data.i = sensorValue.un.rotationVector.i;
+                data.j = sensorValue.un.rotationVector.j;
+                data.k = sensorValue.un.rotationVector.k;
+                validOrien++;
+                count++;
+                break;
+        }
+    } else {
+        data.orien_cali_status = -1; // for bad data or specifying that now the data is slightly old
+    }
+}
+
+void setReports(sh2_SensorId_t reportType, long report_interval) {
+    Serial.println("Setting desired reports");
+    if (! bno08x.enableReport(reportType, report_interval)) {
+      Serial.println("Could not enable rotation vector");
+    }
+}
+
 
 void logData(AllData &data) {
     // Amount of data in ringBuf.
@@ -137,7 +222,11 @@ void logData(AllData &data) {
     rb.write(',');
     rb.print(data.imu.k, 5);
     rb.write(',');
-    rb.println(data.imu.cali_status);
+    rb.print(data.imu.temp);
+    rb.write(',');
+    rb.print(data.imu.new_accel);
+    rb.write(',');
+    rb.println(data.imu.orien_cali_status);
     if (rb.getWriteError()) {
         // Error caused by too few free bytes in RingBuf.
         Serial.println("WriteError");
@@ -190,34 +279,5 @@ void setupSD(){
     }
     // initialize the RingBuf.
     rb.begin(&file);
-}
-
-void readBNO085(IMUData &data) {
-    if (bno08x.wasReset()) {
-        Serial.println("Sensor was reset ");
-        setReports(SH2_ROTATION_VECTOR, 1000);
-        delay(300);
-    }
-    if (bno08x.getSensorEvent(&sensorValue)) {
-        data.cali_status = sensorValue.status;
-        switch (sensorValue.sensorId) {
-            case SH2_ROTATION_VECTOR:
-                data.real = sensorValue.un.rotationVector.real;
-                data.i = sensorValue.un.rotationVector.i;
-                data.j = sensorValue.un.rotationVector.j;
-                data.k = sensorValue.un.rotationVector.k;
-                count++;
-                break;
-        }
-    } else {
-        data.cali_status = -1; // for bad data or specifying that now the data is slightly old
-    }
-}
-
-void setReports(sh2_SensorId_t reportType, long report_interval) {
-    Serial.println("Setting desired reports");
-    if (! bno08x.enableReport(reportType, report_interval)) {
-      Serial.println("Could not enable rotation vector");
-    }
 }
 //============================================ End Functions ============================================
